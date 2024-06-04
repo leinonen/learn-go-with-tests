ed999b858b947a474734a064863461b59adfab5f
Peter Leinonen <peter@leinonen.se>
2024-06-04 20:08:08 +0200

    refactor

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,8 +1,10 @@
 package iteration
 
+const repeatCount = 5
+
 func Repeat(char string) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
+	for i := 0; i < repeatCount; i++ {
 		repeated = repeated + char
 	}
 	return repeated
