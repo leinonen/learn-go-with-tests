e7e99bd960d22e75828265d1aaef81fa0f0488e5
Peter Leinonen <peter@leinonen.se>
2024-06-05 07:25:35 +0200

    triangle and refactor

--- structs-methods-interfaces/shapes.go
+++ structs-methods-interfaces/shapes.go
@@ -26,3 +26,12 @@ func (c Circle) Area() float64 {
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
